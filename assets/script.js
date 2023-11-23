d28c2ed9344ee97006e716ef1adcc93eeca10db1
ElenaTrif <140520572+ElenaTrif@users.noreply.github.com>
2023-11-23 12:05:42 +0100

    Organisation du code js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// * Tableau pour le carrousel avec les images et le texte. * //
+
 const slides = [
 	{
 		"image": "assets/images/slideshow/slide1.jpg",
@@ -17,7 +19,8 @@ const slides = [
 	}
 ]
 
-// * On crée un événement: lorsque on clique sur les flèches, cela gère le changement d'images dans le carrousel. * //
+
+// * Ici se trouvent les variables globales. * //
 
 const bulletsPoints = document.querySelector(".dots");
 let index = 0;
@@ -25,42 +28,9 @@ let flecheLeft = document.getElementById("move_left");
 let flecheRight = document.getElementById("move_right");
 let img = document.querySelector(".banner-img");
 let textImg = document.querySelector("#banner p");
-let carrousel = document.getElementById("banner");
-
-function clicDroite() {
-	flecheRight.addEventListener("click", () => {
-		const slidePoints = document.querySelectorAll(".dots .dot");
-		slidePoints[index].classList.remove("dot_selected");
-		index++
-		if (index > slides.length - 1) {
-			index = 0;
-		}
-		slidePoints[index].classList.add("dot_selected");
-		textImg.innerHTML = slides[index].tagLine;
-		img.src = slides[index].image;
-	})
-}
-clicDroite()
-
-function clicGauche() {
-	flecheLeft.addEventListener("click", () => {
-		const slidePoints = document.querySelectorAll(".dots .dot");
-		slidePoints[index].classList.remove("dot_selected");
-		index--
-		if (index < 0) {
-			index = slides.length - 1;
-		}
-		slidePoints[index].classList.add("dot_selected");
-		textImg.innerHTML = slides[index].tagLine;
-		img.src = slides[index].image;
-	})
-}
-clicGauche()
 
 
 // * On ajoute les bullets points dans le carrousel * //
-
-
 
 function apparitionPoints() {
 	for (let i = 0; i < slides.length; i++) {
@@ -77,3 +47,37 @@ function apparitionPoints() {
 apparitionPoints()
 
 
+// * Changement d'images et de texte et changement de point actif au clic droit. * //
+
+function clicDroite() {
+	flecheRight.addEventListener("click", () => {
+		const slidePoints = document.querySelectorAll(".dots .dot");
+		slidePoints[index].classList.remove("dot_selected");
+		index++
+		if (index > slides.length - 1) {
+			index = 0;
+		}
+		slidePoints[index].classList.add("dot_selected");
+		textImg.innerHTML = slides[index].tagLine;
+		img.src = slides[index].image;
+	})
+}
+clicDroite()
+
+
+// * Changement d'images et de texte et changement de point actif au clic gauche. * //
+
+function clicGauche() {
+	flecheLeft.addEventListener("click", () => {
+		const slidePoints = document.querySelectorAll(".dots .dot");
+		slidePoints[index].classList.remove("dot_selected");
+		index--
+		if (index < 0) {
+			index = slides.length - 1;
+		}
+		slidePoints[index].classList.add("dot_selected");
+		textImg.innerHTML = slides[index].tagLine;
+		img.src = slides[index].image;
+	})
+}
+clicGauche()
